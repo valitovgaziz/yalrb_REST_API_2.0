89f01c2a05fd03ba0d8e21164190237032f51950
valitovgaziz <valitovgaziz@gmail.com>
2023-02-28 10:43:09 +0500

    Add age range entity

---
+++ src/main/java/ru/yalrb/entity/models/AgeRange.java
@@ -0,0 +1,27 @@
+package ru.yalrb.entity.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class AgeRange {
+    @Id
+    @GeneratedValue
+    private Byte id;
+
+    private Integer fromAge;
+
+    private Integer toAge;
+
+    private String name;
+
+}
